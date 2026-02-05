52235955406834c00304a34e42f8fe881aaa8711
jaehyeok <hyukjjang02@gmail.com>
2026-02-05 15:56:50 +0900

    save

--- src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
+++ src/main/java/com/nhnacademy/customer/generator/CustomerGenerator.java
@@ -17,7 +17,6 @@ import com.devskiller.jfairy.producer.person.Person;
 import com.nhnacademy.customer.domain.Customer;
 import com.nhnacademy.nhnmart.entring.EnteringQueue;
 import lombok.extern.slf4j.Slf4j;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -26,39 +25,59 @@ import java.util.concurrent.atomic.AtomicLong;
 @Slf4j
 public class CustomerGenerator implements Runnable {
 
-    //NhnMart 입장 대기열
-    private EnteringQueue enteringQueue;
+    // NhnMart 입장 대기열
+    private final EnteringQueue enteringQueue;
 
-    //회원 번호 Id 생성
-    private AtomicLong atomicId;
+    // 회원 번호 Id 생성
+    private final AtomicLong atomicId;
 
-    //회원이 보유한 default money
-    private final static int DEFAULT_MONEY=10_00000;
+    // 회원이 보유한 default money (1,000,000원)
+    private final static int DEFAULT_MONEY = 1_000_000;
 
     public CustomerGenerator(EnteringQueue enteringQueue) {
         // TODO#4-1 enteringQueue null이면 'IllegalArgumentException'이 발생하는지 검증합니다.
+        if (enteringQueue == null) {
+            throw new IllegalArgumentException("EnteringQueue cannot be null");
+        }
 
         // TODO#4-2 enteringQueue, atomicId를 0으로 초기화합니다.
-
+        this.enteringQueue = enteringQueue;
+        this.atomicId = new AtomicLong(0);
     }
 
     @Override
     public void run() {
-
         /* TODO#4-4 generate() 메서드를 이용해서 customer를 생성하고 enteringQueue에 등록합니다.
-            - while 조건을 수정하세요.
+            - while 조건을 수정하세요. (스레드가 인터럽트 될 때까지 반복)
             - 1초 간격으로 회원을 enteringQueue의 대기열에 등록합니다.
         */
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Customer customer = generate();
+                enteringQueue.addCustomer(customer);
+                log.info("Customer generated and added to queue: {}", customer.getId());
+
+                // 1초 대기
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                log.error("CustomerGenerator interrupted", e);
+                Thread.currentThread().interrupt(); // 인터럽트 상태 복구
+            }
+        }
     }
 
-    private Customer generate(){
-
-                 /* TODO#4-3 Customer 객체를 생성 후 반환합니다.
+    private Customer generate() {
+        /* TODO#4-3 Customer 객체를 생성 후 반환합니다.
             - Customer 객체의 id는 atomicId를 사용하여 구현
             - 회원이름은 랜덤으로 생성됩니다.
                - 회원이름 생성 시 https://github.com/Devskiller/jfairy 이용해서 구현합니다.
          */
+        Fairy fairy = Fairy.create();
+        Person person = fairy.person();
 
-        return null;
+        long id = atomicId.incrementAndGet(); // ID 1씩 증가
+        String name = person.getFullName();   // 랜덤 이름 생성
+
+        return new Customer(id, name, DEFAULT_MONEY);
     }
 }
