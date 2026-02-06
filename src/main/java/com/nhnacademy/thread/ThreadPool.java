122ba001cba087617d370521c0ddb59ca01c05c9
jaehyeok <hyukjjang02@gmail.com>
2026-02-06 10:58:21 +0900

    save

--- src/main/java/com/nhnacademy/thread/ThreadPool.java
+++ src/main/java/com/nhnacademy/thread/ThreadPool.java
@@ -40,18 +40,24 @@ public class ThreadPool {
 
     public ThreadPool(int poolSize, Runnable runnable) {
         // TODO#8-1-2 Thread Pool Size < 0 이라면 IllegalArgumentException이 발생합니다.
-
+        if (poolSize < 0) {
+            throw new IllegalArgumentException("Pool size cannot be negative");
+        }
 
         // TODO#8-1-3 runnable == null 이면 IllegalArgumentException이 발생합니다.
-
+        if (Objects.isNull(runnable)) {
+            throw new IllegalArgumentException("Runnable cannot be null");
+        }
 
         // TODO#8-1-4 runnable이 Runnable의 구현체가 아니라면 IllegalArgumentException이 발생합니다.
-
+        if (!(runnable instanceof Runnable)) {
+            throw new IllegalArgumentException("Object is not an instance of Runnable");
+        }
 
         // TODO#8-1-5 poolSize, runnable, threadList 초기화
-        this.poolSize = 0;
-        this.runnable = null;
-        this.threadList = null;
+        this.poolSize = poolSize;
+        this.runnable = runnable;
+        this.threadList = new ArrayList<>(poolSize);
 
         createThread();
     }
@@ -61,12 +67,22 @@ public class ThreadPool {
           - Thread가 생성되는 과정은 동기화되어야 합니다.
           - mutex, semaphore, synchronized 등등.. 적절히 구현합니다.
         */
+        synchronized (this) {
+            for (int i = 0; i < poolSize; i++) {
+                Thread thread = new Thread(runnable);
+                // 디버깅을 위해 스레드 이름 설정 (선택사항)
+                thread.setName("Worker-Thread-" + i);
+                threadList.add(thread);
+            }
+        }
 
     }
 
     public synchronized void start(){
         // TODO#8-1-7 생성된 Thread를 시작합니다.
-
+        for (Thread thread : threadList) {
+            thread.start();
+        }
     }
 
     public synchronized void stop(){
@@ -74,9 +90,20 @@ public class ThreadPool {
             - Thread가 종료되는 과정에서 동기화되어야 합니다.
             - 우선 모든 Thread interrupt 호출
          */
+        for (Thread thread : threadList) {
+            thread.interrupt();
+        }
 
 
         // TODO#8-1-9 join()를 이용해서 모든 Thread가 종료될 때까지 대기 상태로 만듭니다.
-
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                // 대기 중인 현재 스레드가 인터럽트 되었을 경우, 인터럽트 상태를 복구하고 로그를 남깁니다.
+                Thread.currentThread().interrupt();
+                log.error("Thread interrupted while waiting for thread termination: {}", thread.getName(), e);
+            }
+        }
     }
 }
